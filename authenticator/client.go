c4457ba0d4f34d01313ca4c46096d9730d6b1734
Timothy Nguyen <nguyen.d.timothy@gmail.com>
2020-05-19 16:32:08 -0700

    added to test grpc client

--- authenticator/client.go
+++ authenticator/client.go
@@ -1,15 +1,38 @@
 package main
 
 import (
+	"context"
+	pb "dbauth/authenticator/messages"
 	"fmt"
+
 	"google.golang.org/grpc"
 )
 
 func main() {
-	conn, err := grpc.Dial("authenticator:1234", grpc.WithInsecure())
+	fmt.Printf("enter client test\n")
+
+	conn, err := grpc.Dial("localhost:1234", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("connected\n")
-	defer conn.Close()
+	fmt.Printf("dialed grpc server\n")
+
+	client := pb.NewAuthenticatorClient(conn)
+	ctx := context.Background()
+	req := pb.AuthenticateRequest{
+		Identity: "diotim",
+		Salt:     "swag",
+	}
+	resp, err := client.Authenticate(ctx, &req)
+	if err != nil {
+		panic(fmt.Sprintf("failed to authenticate: %v", err))
+	}
+	fmt.Printf("made grpc request\n")
+
+	creds := resp.GetCredentials()
+	if creds == nil {
+		panic("no credentials returned")
+	}
+
+	fmt.Printf("\nuser: %s\thashedPassword: %s\n", creds.User, creds.HashedPassword)
 }
