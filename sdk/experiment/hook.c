8fc3aa4675def80beced244a480bbbbedd08ef69
Dio Gado <ahmedehabg@gmail.com>
2020-05-29 02:21:06 -0400

    cleanup code

--- sdk/experiment/hook.c
+++ sdk/experiment/hook.c
@@ -12,7 +12,7 @@ extern void PQconnectStart();
 extern void PQlibVersion();
 extern void PQprotocolVersion();
 extern void PQfireResultCreateEvents();
-extern void PQencryptPassword();
+extern char *PQencryptPassword(const char *passwd, const char *user);
 
 void print_source(char *p, int offset, int nbytes);
 static int unprotect_page(void* addr);
@@ -26,24 +26,30 @@ void register_python_callback(python_md5_func *_callback) {
 
 bool our_md5_encrypt(const char *passwd, const char *salt, size_t salt_len,
         char *buf) {
-    const unsigned char *usalt = (const unsigned char *)salt;
-    fprintf(stderr, "libhook(our_md5_encrypt): salt (%ld bytes) is %02x,%02x,%02x,%02x\n",
-            salt_len, usalt[0], usalt[1], usalt[2], usalt[3]);
+    char *salt_string;
+    char *hash;
+    salt_string = (char *) malloc(salt_len+1);
+    memcpy(salt_string, salt, salt_len);
+    salt_string[salt_len] = '\0';
+    fprintf(stderr, "libhook(our_md5_encrypt): salt (%ld bytes) is ", salt_len);
+    for (int i=0; i < salt_len; i++) {
+        fprintf(stderr, "%02x", (unsigned char) salt_string[i]);
+    }
+    fprintf(stderr, "\n");
     if (callback != NULL) {
-        char *salt_null_terminated = (char *) malloc(salt_len+1);
-        memcpy(salt_null_terminated, salt, salt_len);
-        salt_null_terminated[salt_len] = '\0';
-        char *received_hash = (*callback)(salt_null_terminated);
-        free(salt_null_terminated);
-    fprintf(stderr, "libhook(our_md5_encrypt): received hash %s\n", received_hash);
+        hash = (*callback)(salt_string);
+        if (strlen(hash) > 0) {
+            fprintf(stderr, "libhook(our_md5_encrypt): received hash %s\n", hash);
+        } else {
+            fprintf(stderr, "libhook(our_md5_encrypt): received empty hash, reverting to libpq hash\n");
+            hash = PQencryptPassword(passwd, salt_string);
+            if (!hash)
+                return false;
+        }
     }
-    buf[0] = 'd';
-    buf[1] = 'b';
-    buf[2] = 'a';
-    for (char i =3; i < 35; i++) {
-        buf[i] = i;
-    }
-    buf[35] = '\0';
+    memcpy(buf, hash, strlen(hash));
+    free(hash);
+    return true;
 }
 
 char *hook2() {
@@ -91,101 +97,9 @@ char *hook2() {
     return NULL;
 }
 
-char *hook1() {
-    // this hook shows that we can overrite the machine instructions of a
-    // function to change its return value. in this example, the (exported) 
-    // function PQProtocolVersion is changed so that its returns 420, which is
-    // not a possible protocol number. this works. the next challenge is to change
-    // that is not public.
-
-    char *p1 = (char *) &PQprotocolVersion;
-    int index = 0x14;
-    char overwrite[] = {0xb8, 0xa4, 0x01, 0x00, 0x00, 0x90, 0x90};
-	if(unprotect_page(p1)) {
-		fprintf(stderr, "Could not unprotect mem: %p\n", p1);
-		return NULL;
-	}
-    for (int i=0; i<sizeof(overwrite); i++) {
-        p1[index+i] = overwrite[i];
-    }
-    return NULL;
-}
-
-void print_source(char *p, int offset, int nbytes) {
-    for (int i=offset; i < offset+nbytes; i++) {
-        if (i == 0)
-            fprintf(stderr, "|");
-        fprintf(stderr, "%02x ", (unsigned char) p[i]);
-    }
-    fprintf(stderr, "\n");
-}
-
-
-int sanity_check1() {
-    // this checks that at least for these two functions, the relative addresses
-    // do not change at different runtimes. if they do, that means that the
-    // dynamic linking is doing more than a simple memory map, or that there is
-    // something else funky going on.
-    // this used to be true for apt libpq-dev but isnot for compiled libpq
-    void *p1 = &PQconnectStart;
-    void *p2 = &PQlibVersion;
-    fprintf(stderr, "%p\n ", p2);
-    return (p2-p1) == 18640;
-}
-
-/* TODO: make less ugly! 
- *       there's got to be a nicer way to do this! */
-#pragma pack(push, 1)
-static struct { 
-	char push_rax; 
-	char mov_rax[2];
-	char addr[8];
-	char jmp_rax[2]; } 
-jump_asm = {
-	.push_rax = 0x50,
-	.mov_rax  = {0x48, 0xb8},
-	.jmp_rax  = {0xff, 0xe0} };
-#pragma pack(pop)
-
 static int unprotect_page(void* addr) {
-	int pagesize = sysconf(_SC_PAGE_SIZE);
-	int pagemask = ~(pagesize -1);
-	char* page = (char *)((size_t)addr & pagemask);
-	return mprotect(page, pagesize, PROT_READ | PROT_WRITE | PROT_EXEC);
-}
-
-int hook_function(void* target, void* replace) {
-	int count;
-
-	if(unprotect_page(replace)) {
-		fprintf(stderr, "Could not unprotect replace mem: %p\n", replace);
-		return 1;
-	}
-
-	if(unprotect_page(target)) {
-		fprintf(stderr, "Could not unprotect target mem: %p\n", target);
-		return 1;
-	}
-
-	/* find the NOP */
-	for(count = 0; count < 255 && ((unsigned char*)replace)[count] != 0x90; ++count);
-
-	if(count == 255) {
-		fprintf(stderr, "Couldn't find the NOP.\n");
-		return 1;
-	}
-
-	/* shift everything down one */
-	memmove(replace+1, replace, count);
-
-	/* add in `pop %rax` */
-	*((unsigned char *)replace) = 0x58;
-
-	/* set up the address */
-	memcpy(jump_asm.addr, &replace, sizeof (void *));
-
-	/* smash the target function */
-	memcpy(target, &jump_asm, sizeof jump_asm);
-
-	return 0;
+    int pagesize = sysconf(_SC_PAGE_SIZE);
+    int pagemask = ~(pagesize -1);
+    char* page = (char *)((size_t)addr & pagemask);
+    return mprotect(page, pagesize, PROT_READ | PROT_WRITE | PROT_EXEC);
 }
--- sdk/experiment/main.py
+++ sdk/experiment/main.py
@@ -8,19 +8,15 @@ import psycopg2 as pg2
 libpq = CDLL("libpq.so", mode=RTLD_GLOBAL)
 libhook = CDLL(osp.abspath("libhook.so"), mode=RTLD_GLOBAL)
 
-# assert libhook.sanity_check1() == 1
-
 conn = pg2.connect("")
-assert conn.info.protocol_version == 3
-#libhook.hook1()
-#assert conn.info.protocol_version == 420
 
 libhook.hook2()
-salt_ = None
+
 @CFUNCTYPE(c_char_p, c_char_p)
 def md5_callback(salt):
     print('main.py(md5_callback): salt is', salt.hex())
-    return "sheesh".encode()
+    hash = "".encode()
+    return hash
 
 libhook.register_python_callback(md5_callback)
 conn = pg2.connect("")
