82521121466d3552d96a3801fc0915f3bd7bcb5e
Dio Gado <ahmedehabg@gmail.com>
2020-06-03 03:14:18 -0400

    remove examples folder

--- examples/client.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import psycopg2
-
-conn = psycopg2.connect(host='db', user='bob', password='password', dbname='db')
-print('Connection established')
--- examples/dbauth_client.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import psycopg2
-
-conn = psycopg2.connect(host='db', authenticator='authenticator:1234', identity='diotim', dbname='db')
-print('Connection established')
