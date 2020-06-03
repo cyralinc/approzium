4e120a926788a7d1412d385130720ce15d8b7681
Dio Gado <ahmedehabg@gmail.com>
2020-06-03 04:09:42 -0400

    add setup.py

---
+++ sdk/python/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+long_description = open('README.md', 'r').read()
+
+
+setuptools.setup(
+        name='approzium',
+        description='Python SDK for Approzium',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        url='https://github.com/approzium/approzium/',
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+        install_requires=[
+            'psycopg2',
+            'boto3',
+            'grpcio'
+        ]
+)
