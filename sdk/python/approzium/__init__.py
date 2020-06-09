ed5e5f2a9fdfa822e23adf8a639795bf0b3e9d39
Dio Gado <ahmedehabg@gmail.com>
2020-06-09 13:11:30 -0400

    Change authenticator and iam_role to module-level attributes

--- sdk/python/approzium/__init__.py
+++ sdk/python/approzium/__init__.py
@@ -1,7 +1,21 @@
 import logging
 
+authenticator_addr = "authenticator:1234"
+iam_role = None
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="[%(filename)s:%(lineno)s - %(funcName)10s() ] %(message)s",
 )
+
+
+def set_authenticator(new_authenticator):
+    global authenticator_addr
+    authenticator_addr = new_authenticator
+
+
+def set_iam_role(new_iam_role):
+    # this function tells Approzium to assume an IAM role and use that as its identity
+    # with the authenticator service
+    global iam_role
+    iam_role = new_iam_role
