ccd9f2efd03a87ee1ca9d9da9b2b0e687b62a212
Dio Gado <ahmedehabg@gmail.com>
2020-06-12 16:11:46 -0400

    Add debug statements in client.py

--- sdk/python/client.py
+++ sdk/python/client.py
@@ -1,9 +1,18 @@
 import psycopg2
 from approzium.psycopg2 import connect
 from approzium import set_authenticator, set_iam_role
+import logging
+
+logger = logging.getLogger('approzium')
+logger.setLevel(logging.DEBUG)
 
 
 set_iam_role('arn:aws:iam::accountid:role/rolename')
 set_authenticator('authenticator:1234')
 conn = connect("")
 print('Connection Established')
+
+def test():
+    conn.cursor().execute('SELECT 1')
+
+test()
