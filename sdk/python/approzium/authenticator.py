92c152835d0922e7cba171c563e04901cf623cd4
Dio Gado <ahmedehabg@gmail.com>
2020-06-18 21:58:40 -0400

    Revert "ignore ResoruceWarning in GCI call"
    This reverts commit 78c07d534160359caefe3ff6e9e30d7bc1a10894.

--- sdk/python/approzium/authenticator.py
+++ sdk/python/approzium/authenticator.py
@@ -3,7 +3,6 @@ import logging
 import hashlib
 import grpc
 from pathlib import Path
-import warnings
 
 # needed to be able to import protos code
 import sys
@@ -20,40 +19,38 @@ class Authenticator(object):
         self.iam_role = iam_role
 
 def get_hash(dbhost, dbport, dbuser, auth_type, auth_info, authenticator):
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore", ResourceWarning)
-        signed_gci = obtain_signed_get_caller_identity(authenticator.iam_role)
-    with grpc.insecure_channel(authenticator.address) as channel:
-        stub = authenticator_pb2_grpc.AuthenticatorStub(channel)
+    signed_gci = obtain_signed_get_caller_identity(authenticator.iam_role)
+    channel = grpc.insecure_channel(authenticator.address)
+    stub = authenticator_pb2_grpc.AuthenticatorStub(channel)
 
-        if auth_type == approzium.psycopg2.AUTH_REQ_MD5:
-            salt = auth_info
-            if len(salt) != 4:
-                raise Exception("salt not right size")
-            request = authenticator_pb2.PGMD5HashRequest(
-                signed_get_caller_identity=signed_gci,
-                claimed_iam_arn=authenticator.iam_role,
-                dbhost=dbhost,
-                dbuser=dbuser,
-                dbport=dbport,
-                salt=salt,
-            )
-            response = stub.GetPGMD5Hash(request)
-            return response.hash
-        elif auth_type == approzium.psycopg2.AUTH_REQ_SASL:
-            auth = auth_info
-            auth._generate_auth_msg()
-            request = authenticator_pb2.PGSHA256HashRequest(
-                signed_get_caller_identity=signed_gci,
-                claimed_iam_arn=authenticator.iam_role,
-                dbhost=dbhost,
-                dbport=dbport,
-                dbuser=dbuser,
-                salt=auth.password_salt,
-                iterations=auth.password_iterations,
-                authentication_msg=auth.authorization_message
-            )
-            response = stub.GetPGSHA256Hash(request)
-            client_final = auth.create_client_final_message(response.cproof)
-            auth.server_signature = response.sproof
-            return client_final, auth
+    if auth_type == approzium.psycopg2.AUTH_REQ_MD5:
+        salt = auth_info
+        if len(salt) != 4:
+            raise Exception("salt not right size")
+        request = authenticator_pb2.PGMD5HashRequest(
+            signed_get_caller_identity=signed_gci,
+            claimed_iam_arn=authenticator.iam_role,
+            dbhost=dbhost,
+            dbuser=dbuser,
+            dbport=dbport,
+            salt=salt,
+        )
+        response = stub.GetPGMD5Hash(request)
+        return response.hash
+    elif auth_type == approzium.psycopg2.AUTH_REQ_SASL:
+        auth = auth_info
+        auth._generate_auth_msg()
+        request = authenticator_pb2.PGSHA256HashRequest(
+            signed_get_caller_identity=signed_gci,
+            claimed_iam_arn=authenticator.iam_role,
+            dbhost=dbhost,
+            dbport=dbport,
+            dbuser=dbuser,
+            salt=auth.password_salt,
+            iterations=auth.password_iterations,
+            authentication_msg=auth.authorization_message
+        )
+        response = stub.GetPGSHA256Hash(request)
+        client_final = auth.create_client_final_message(response.cproof)
+        auth.server_signature = response.sproof
+        return client_final, auth
