e6f58091b1cc5cdb54a4793b50594e5c41f36496
Dio Gado <ahmedehabg@gmail.com>
2020-06-19 18:09:28 -0400

    update client to match

--- sdk/python/client.py
+++ sdk/python/client.py
@@ -1,20 +1,14 @@
 from os import environ
-from approzium import Authenticator, set_default_authenticator
+from approzium import AuthClient
 from approzium.psycopg2 import connect
 import logging
+from pprint import pprint
 
-logger = logging.getLogger("approzium")
-logger.setLevel(logging.DEBUG)
-
-
-auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
-set_default_authenticator(auth)
-conn = connect("")  # or connect("", authenticator=auth)
+auth = AuthClient("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
+conn = connect("", auth=auth)
 print("Connection Established")
-
-
-def test():
-    conn.cursor().execute("SELECT 1")
-
-
-test()
+# TODO
+# add add_attribution_info(key, value)
+# capture env vars with certain prefix, names
+# try integration with some tracing libs
+pprint(auth.attribution_info)
