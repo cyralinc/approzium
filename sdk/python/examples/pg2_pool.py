e699400b964a073916ec59249fa003595c0efebc
Dio Gado <ahmedehabg@gmail.com>
2020-06-19 16:22:14 -0400

    remove unused import

--- sdk/python/examples/pg2_pool.py
+++ sdk/python/examples/pg2_pool.py
@@ -1,5 +1,5 @@
 from os import environ
-from approzium import Authenticator, set_default_authenticator
+from approzium import Authenticator
 from approzium.psycopg2.pool import ThreadedConnectionPool
 
 auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
