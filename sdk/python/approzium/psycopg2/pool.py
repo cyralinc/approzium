068cfd4eb443228e14f0be5db1634a59bf3459f3
Dio Gado <ahmedehabg@gmail.com>
2020-06-19 00:30:13 -0400

    Add pool.py

---
+++ sdk/python/approzium/psycopg2/pool.py
@@ -0,0 +1,21 @@
+from . import connect
+import psycopg2.pool
+
+
+class AbstractConnectionPool(psycopg2.pool.AbstractConnectionPool):
+    def _connect(self, key=None):
+        # override connect method
+        conn = connect(*self._args, **self._kwargs)
+        if key is not None:
+            self._used[key] = conn
+            self._rused[id(conn)] = key
+        else:
+            self._pool.append(conn)
+        return conn
+
+
+class SimpleConnectionPool(AbstractConnectionPool, psycopg2.pool.SimpleConnectionPool):
+    pass
+
+class ThreadedConnectionPool(AbstractConnectionPool, psycopg2.pool.ThreadedConnectionPool):
+    pass
