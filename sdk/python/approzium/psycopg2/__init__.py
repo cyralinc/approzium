ac439b38646ffd0bd18a8aec8d280816b72a9c06
UpGado <UpGado@users.noreply.github.com>
2020-06-19 20:36:06 +0000

    Automatic lint
    Signed-off-by: GitHub Actions Bot <actions@github.com>

--- sdk/python/approzium/psycopg2/__init__.py
+++ sdk/python/approzium/psycopg2/__init__.py
@@ -1,4 +1,4 @@
 from ._psycopg2_connect import connect, AUTH_REQ_MD5, AUTH_REQ_SASL
 
 
-__all__ = ['connect', 'AUTH_REQ_MD5', 'AUTH_REQ_SASL']
+__all__ = ["connect", "AUTH_REQ_MD5", "AUTH_REQ_SASL"]
