44c5405438f03194566448a6f84564210bbee88b
Dio Gado <ahmedehabg@gmail.com>
2020-06-20 14:55:35 -0400

    add asnyc_ to test matrix

--- sdk/python/tests/test_psycopg2_connect.py
+++ sdk/python/tests/test_psycopg2_connect.py
@@ -1,4 +1,6 @@
+from select import select
 from os import environ
+import psycopg2
 
 import pytest
 from approzium import Authenticator
@@ -13,10 +15,31 @@ connopts = {
 }
 
 
-@pytest.mark.parametrize("sslmode", ["require", "disable"])
+# waits for an async connection to start or finish execution
+# source: https://www.psycopg.org/docs/advanced.html
+def wait(conn):
+    while True:
+        state = conn.poll()
+        if state == psycopg2.extensions.POLL_OK:
+            break
+        elif state == psycopg2.extensions.POLL_WRITE:
+            select([], [conn.fileno()], [])
+        elif state == psycopg2.extensions.POLL_READ:
+            select([conn.fileno()], [], [])
+        else:
+            raise psycopg2.OperationalError("poll() returned %s" % state)
+
+
 @pytest.mark.parametrize("dbhost", ["dbmd5", "dbsha256"])
-def test_connect(dbhost, sslmode):
-    conn = connect(**connopts, host=dbhost, sslmode=sslmode, authenticator=auth)
+@pytest.mark.parametrize("sslmode", ["require", "disable"])
+@pytest.mark.parametrize("async_", [1, 0])
+def test_connect(dbhost, sslmode, async_):
+    conn = connect(**connopts, host=dbhost, sslmode=sslmode, async_=async_,
+                   authenticator=auth)
+    if async_:
+        wait(conn)
     cur = conn.cursor()
     cur.execute("SELECT 1")
+    if async_:
+        wait(conn)
     assert cur.fetchone() == (1,)
