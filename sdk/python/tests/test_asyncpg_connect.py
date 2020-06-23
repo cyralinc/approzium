acdd896a1e501b099088d3d875e901d4ab5d98f3
Dio Gado <ahmedehabg@gmail.com>
2020-06-23 13:54:30 -0400

    add tests for asyncpg support

---
+++ sdk/python/tests/test_asyncpg_connect.py
@@ -0,0 +1,25 @@
+from os import environ
+
+import pytest
+from approzium import Authenticator
+from approzium.asyncpg import connect
+
+auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
+# use Psycopg2 defined test environment variables
+connopts = {
+    "user": environ["PSYCOPG2_TESTDB_USER"],
+    "database": environ["PSYCOPG2_TESTDB"],
+    "port": int(environ["PSYCOPG2_TESTDB_PORT"]),
+}
+
+
+@pytest.mark.parametrize("host", ["dbmd5", "dbsha256"])
+@pytest.mark.parametrize("sslmode", ['disable', 'require'])
+@pytest.mark.asyncio
+async def test_connect(host, sslmode):
+    # set SSL mode using env variable because there is no better way
+    environ['PGSSLMODE'] = sslmode
+    conn = await connect(
+        **connopts, host=host, authenticator=auth
+    )
+    await conn.fetch("SELECT 1")
