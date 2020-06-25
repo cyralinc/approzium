3d296200a45a106fe170fa653cfb3403675b43bf
UpGado <UpGado@users.noreply.github.com>
2020-06-25 03:12:48 +0000

    Automatic lint
    Signed-off-by: GitHub Actions Bot <actions@github.com>

--- sdk/python/examples/asyncpg_pool.py
+++ sdk/python/examples/asyncpg_pool.py
@@ -8,8 +8,9 @@ auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE")
 
 
 async def run():
-    pool = await create_pool(user="bob", database="db", host="dbmd5",
-                             authenticator=auth)
+    pool = await create_pool(
+        user="bob", database="db", host="dbmd5", authenticator=auth
+    )
     print("Connection Established!")
     async with pool.acquire() as conn:
         await conn.fetch("""SELECT 1""")
