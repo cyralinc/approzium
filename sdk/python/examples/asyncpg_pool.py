f8215aa55b7194319d5da5f611cfc3f7578ac546
Dio Gado <ahmedehabg@gmail.com>
2020-06-24 23:08:57 -0400

    Add asyncpg pool functionality

---
+++ sdk/python/examples/asyncpg_pool.py
@@ -0,0 +1,19 @@
+import asyncio
+from os import environ
+
+from approzium import Authenticator
+from approzium.asyncpg.pool import create_pool
+
+auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
+
+
+async def run():
+    pool = await create_pool(user="bob", database="db", host="dbmd5",
+                             authenticator=auth)
+    print("Connection Established!")
+    async with pool.acquire() as conn:
+        await conn.fetch("""SELECT 1""")
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(run())
--- sdk/python/tests/test_asyncpg_connect.py
+++ sdk/python/tests/test_asyncpg_connect.py
@@ -3,6 +3,7 @@ from os import environ
 import pytest
 from approzium import Authenticator
 from approzium.asyncpg import connect
+from approzium.asyncpg.pool import create_pool
 
 auth = Authenticator("authenticator:1234", iam_role=environ.get("TEST_IAM_ROLE"))
 # use Psycopg2 defined test environment variables
@@ -21,3 +22,14 @@ async def test_connect(host, sslmode):
     environ["PGSSLMODE"] = sslmode
     conn = await connect(**connopts, host=host, authenticator=auth)
     await conn.fetch("SELECT 1")
+
+
+@pytest.mark.parametrize("host", ["dbmd5", "dbsha256"])
+@pytest.mark.parametrize("sslmode", ["disable", "require"])
+@pytest.mark.asyncio
+async def test_pool(host, sslmode):
+    # set SSL mode using env variable because there is no better way
+    environ["PGSSLMODE"] = sslmode
+    pool = await create_pool(**connopts, host=host, authenticator=auth)
+    async with pool.acquire() as conn:
+        await conn.fetch("""SELECT 1""")
