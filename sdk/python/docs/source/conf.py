5f0cb5608add4b3703c01912a0679f28022f934e
Dio Gado <ahmedehabg@gmail.com>
2020-06-25 22:20:55 -0400

    change theme to default

--- sdk/python/docs/source/conf.py
+++ sdk/python/docs/source/conf.py
@@ -27,7 +27,7 @@ author = "Cyral"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -43,7 +43,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "default"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
