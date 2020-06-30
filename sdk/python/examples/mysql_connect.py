c6eca9239db8869f0f0211567f3c3372f11b3b79
Dio Gado <ahmedehabg@gmail.com>
2020-06-30 03:11:00 -0400

    Add example for MySQL connector

---
+++ sdk/python/examples/mysql_connect.py
@@ -0,0 +1,14 @@
+from os import environ
+
+import approzium
+from approzium.mysql.connector import connect
+
+auth = approzium.AuthClient("authenticator:6000", iam_role=environ.get("TEST_IAM_ROLE"))
+approzium.default_auth_client = auth
+conn = connect(user='bob', authenticator=auth,
+               host='dbmysql', use_pure=True)
+print("Connection Established")
+cur = conn.cursor()
+cur.execute('SELECT 1')
+result = next(cur)
+print(result)
