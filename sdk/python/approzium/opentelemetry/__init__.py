20d33451f7db193c4d9652002b437a6e4b27b560
Dio Gado <ahmedehabg@gmail.com>
2020-07-02 11:23:23 -0400

    Add approzium.opentelemetry

---
+++ sdk/python/approzium/opentelemetry/__init__.py
@@ -0,0 +1,33 @@
+import opentelemetry.ext.dbapi
+from opentelemetry.ext.dbapi import DatabaseApiIntegration
+
+
+original_DatabaseApiIntegration = None
+
+class ApproziumDatabaseApiIntegration(DatabaseApiIntegration):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def get_connection_attributes(self, connection):
+        super().get_connection_attributes(connection)
+        # check if this is an Approzium connection
+        if hasattr(connection, 'authenticator'):
+            self.add_approzium_span_attributes(connection)
+
+    def add_approzium_span_attributes(self, connection):
+        authenticator = connection.authenticator
+        for key, value in connection.authenticator.attribution_info.items():
+            self.span_attributes['approzium.' + key] = value
+
+
+def instrument():
+    global original_DatabaseApiIntegration
+    original_DatabaseApiIntegration = DatabaseApiIntegration
+    opentelemetry.ext.dbapi.DatabaseApiIntegration = ApproziumDatabaseApiIntegration
+
+
+def uninstrument():
+    if original_DatabaseApiIntegration is None:
+        raise Exception('Opentelemetry is not currently instrumented')
+    opentelemetry.ext.dbapi.DatabaseApiIntegration = \
+    original_DatabaseApiIntegration
