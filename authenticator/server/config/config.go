719a385e5c937f2ea694f51dd8a372ff9275302e
Ahmed Gado <ahmedehabg@gmail.com>
2020-07-08 04:29:13 -0400

    Use cleanenv for config reading

--- authenticator/server/config/config.go
+++ authenticator/server/config/config.go
@@ -1,6 +1,6 @@
 package config
 
-import "github.com/kelseyhightower/envconfig"
+import "github.com/ilyakaznacheev/cleanenv"
 
 // Config is an object for storing configuration variables set through
 // Approzium's environment.
@@ -8,22 +8,22 @@ import "github.com/kelseyhightower/envconfig"
 // Please see https://approzium.org/configuration for elaboration upon
 // each parameter.
 type Config struct {
-	Host     string `default:"127.0.0.1"`
-	HTTPPort int    `envconfig:"http_port" default:"6000"`
-	GRPCPort int    `envconfig:"grpc_port" default:"6001"`
+	Host     string `yaml:"host" env:"APPROZIUM_HOST" env-default:"127.0.0.1"`
+	HTTPPort int    `yaml:"http_port" env:"APPROZIUM_HTTP_PORT" env-default:"6000"`
+	GRPCPort int    `yaml:"grpc_port" env:"APPROZIUM_GRPC_PORT" env-default:"6001"`
 
-	LogLevel  string `envconfig:"log_level" default:"info"`
-	LogFormat string `envconfig:"log_format" default:"text"` // Also supports "json".
-	LogRaw    bool   `envconfig:"log_raw" default:"false"`
+	LogLevel  string `default:"info" yaml:"log_level" env:"APPROZIUM_LOG_LEVEL" env-default:"info"`
+	LogFormat string `yaml:"log_format" env:"APPROZIUM_LOG_FORMAT" env-default:"text"` // Also supports "json".
+	LogRaw    bool   `yaml:"log_raw" env:"APPROZIUM_LOG_RAW" env-default:"false"`
 
-	VaultTokenPath string `envconfig:"vault_token_path"`
+	VaultTokenPath string `yaml:"vault_token_path" env:"APPROZIUM_VAULT_TOKEN_PATH"`
 }
 
 // ParseConfig returns the parsed config. A pointer is not returned
 // because after first parse, the config is immutable.
 func ParseConfig() (Config, error) {
 	var config Config
-	if err := envconfig.Process("approzium", &config); err != nil {
+	if err := cleanenv.ReadEnv(&config); err != nil {
 		return Config{}, err
 	}
 	return config, nil
