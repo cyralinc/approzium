8693660836160135f168ed9e603fb72d51973e68
Becca Petrin <beccapetrin@gmail.com>
2020-07-13 12:16:39 -0700

    Add support for TLS and dev mode (#111)
    * Add support for TLS and dev mode
    
    * Update health check
    
    * Always err when TLS is enabled but no cert/key
    
    * Fix err output
    
    * Changes from feedback
    
    * Fix linting issues
    
    * Automatic lint
    
    Signed-off-by: GitHub Actions Bot <actions@github.com>
    
    * Fix conftest.py
    
    * Fix incorrect parameter
    
    * Continue to fix the trail of typos <sob>
    
    * Add disable_tls param to AuthClient
    
    * Move TLS example
    
    * Remove diff markup
    
    * Add TLS example
    
    Co-authored-by: tyrannosaurus-becks <tyrannosaurus-becks@users.noreply.github.com>

--- authenticator/server/api/api.go
+++ authenticator/server/api/api.go
@@ -1,46 +1,74 @@
 package api
 
 import (
+	"crypto/tls"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
+	"time"
 
 	"contrib.go.opencensus.io/exporter/prometheus"
 	"github.com/cyralinc/approzium/authenticator/server/config"
 	log "github.com/sirupsen/logrus"
 )
 
-func Start(logger *log.Logger, config config.Config) <-chan error {
-	errChan := make(chan error)
+func Start(logger *log.Logger, config config.Config) error {
 
-	endpoints, err := loadEndpoints(logger, config)
-	if err != nil {
-		errChan <- err
-		return errChan
+	if err := loadEndpoints(logger, config); err != nil {
+		return err
 	}
 
 	serviceAddress := config.Host + ":" + strconv.Itoa(config.HTTPPort)
-	logger.Infof("api listening for requests on %s", serviceAddress)
-	go func() {
-		if err := http.ListenAndServe(serviceAddress, endpoints); err != nil {
-			errChan <- err
+	server := &http.Server{
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	if config.DisableTLS {
+		server.Addr = serviceAddress
+		go func() {
+			logger.Fatal(server.ListenAndServe())
+		}()
+		logger.Infof("api starting on http://%s", serviceAddress)
+	} else {
+		server.Addr = fmt.Sprintf(":%d", config.HTTPPort)
+		crt, err := ioutil.ReadFile(config.PathToTLSCert)
+		if err != nil {
+			return err
 		}
-	}()
-	return errChan
+		key, err := ioutil.ReadFile(config.PathToTLSKey)
+		if err != nil {
+			return err
+		}
+		cert, err := tls.X509KeyPair(crt, key)
+		if err != nil {
+			return err
+		}
+		server.TLSConfig = &tls.Config{
+			Certificates: []tls.Certificate{cert},
+			ServerName:   config.Host,
+		}
+
+		go func() {
+			logger.Fatal(server.ListenAndServeTLS("", ""))
+		}()
+		logger.Infof("api starting on https://%s", serviceAddress)
+	}
+	return nil
 }
 
-func loadEndpoints(logger *log.Logger, config config.Config) (*http.ServeMux, error) {
-	mux := http.NewServeMux()
-
+func loadEndpoints(logger *log.Logger, config config.Config) error {
 	prometheusHandler, err := prometheus.NewExporter(prometheus.Options{
 		Namespace: "approzium",
 	})
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	// Alphabetical by endpoint.
-	mux.Handle("/v1/health", newHealthChecker(logger, config))
-	mux.Handle("/v1/metrics/prometheus", prometheusHandler)
+	http.Handle("/v1/health", newHealthChecker(logger, config))
+	http.Handle("/v1/metrics/prometheus", prometheusHandler)
 
-	return mux, nil
+	return nil
 }
