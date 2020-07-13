510c1b9ea51070073c70bdf152e4184a625cf16c
Timothy Nguyen <tnguyen@cyral.com>
2020-07-13 14:59:53 -0400

    Docasaurus (#110)
    * init docasaurus
    
    * transfer existing content from docz to docasaurus
    
    * add .DS_Store to .gitignore
    
    * add architecture diagram
    
    * add logo, change colors
    
    * add @docusaurus/plugin-ideal-image
    
    * add makefile
    
    * remove docasaurus docs
    
    * update link to python client library api
    
    * add roadmap to sidebar
    
    * add security model to docs
    
    * nit pick
    
    * Add external link symbol for Python API doc
    
    * fix broken links on overview page
    
    * remove features
    
    * Add examples page
    
    * udpate overview
    
    * update home page
    
    * add info modal
    
    * update css
    
    * update docasaurus config
    
    * move documentaiton navlink
    
    * swizzle footer and navbar
    
    * swizzle codeblock
    
    * fix info modal styling
    
    * fix info modal interaction
    
    * style code, links
    
    * adjust code margin
    
    * update button color
    
    * update code styling
    
    * add blurb for architecture diagram
    
    * init docasaurus
    
    * transfer existing content from docz to docasaurus
    
    * add .DS_Store to .gitignore
    
    * add architecture diagram
    
    * add logo, change colors
    
    * add @docusaurus/plugin-ideal-image
    
    * add makefile
    
    * remove docasaurus docs
    
    * update link to python client library api
    
    * add roadmap to sidebar
    
    * add security model to docs
    
    * nit pick
    
    * Add external link symbol for Python API doc
    
    * fix broken links on overview page
    
    * Add examples page
    
    * remove features
    
    * udpate overview
    
    * update home page
    
    * add info modal
    
    * update css
    
    * update docasaurus config
    
    * move documentaiton navlink
    
    * swizzle footer and navbar
    
    * swizzle codeblock
    
    * fix info modal styling
    
    * fix info modal interaction
    
    * style code, links
    
    * adjust code margin
    
    * update button color
    
    * add blurb for architecture diagram
    
    * update code styling
    
    * updated configuration docs
    
    Co-authored-by: Dio Gado <ahmedehabg@gmail.com>

--- docs/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-const express = require(`express`)
-
-// Enable development support for serving HTML from `./static` folder
-// reference: https://github.com/gatsbyjs/gatsby/issues/13072
-exports.onCreateDevServer = ({ app }) => {
-    app.use(express.static(`public`))
-}
