f694fb531f680a6cd0fca3e8a5d6c307d9d6eda2
Ahmed Gado <ahmedehabg@gmail.com>
2020-07-15 01:03:32 -0400

    Add docstring and using default authclient

--- sdk/python/approzium/pymysql/__init__.py
+++ sdk/python/approzium/pymysql/__init__.py
@@ -5,4 +5,5 @@ def connect(*args, **kwargs):
     return ApproziumConnection(*args, **kwargs)
 
 
+connect.__doc__ = ApproziumConnection.__init__.__doc__
 __all__ = ["connect"]
--- sdk/python/approzium/pymysql/_connect.py
+++ sdk/python/approzium/pymysql/_connect.py
@@ -1,4 +1,5 @@
 import pymysql
+import approzium
 
 from .._misc import patch
 from .._mysql import MYSQLNativePassword, get_auth_resp
@@ -20,6 +21,30 @@ def _scramble_native_password(context, salt):
 
 class ApproziumConnection(pymysql.connections.Connection):
     def __init__(self, *args, authenticator=None, **kwargs):
+        """Creates a PyMySQL connection through Approzium authentication. Takes
+        the same arguments as ``pymysql.connect``, in addition to the
+        authenticator argument.
+
+        :param authenticator: AuthClient instance to be used for authentication. If
+            not provided, the default AuthClient, if set, is used.
+        :type authenticator: approzium.AuthClient, optional
+        :raises: TypeError, if no AuthClient is given and no default one is set.
+        :rtype: ``pymysql.connections.Connection``
+
+        Example:
+
+        .. code-block:: python
+
+            >>> import approzium
+            >>> from approzium.pymysql import connect
+            >>> auth = approzium.AuthClient("authenticatorhost:6001", disable_tls=True)
+            >>> con = connect("host=DB.com dbname=mydb", authenticator=auth)
+            >>> # use the connection just like any other PyMySQL connection
+        """
+        if authenticator is None:
+            authenticator = approzium.default_auth_client
+        if authenticator is None:
+            raise TypeError("Auth client not specified and not default auth client is set")
         self.authenticator = authenticator
         return super(ApproziumConnection, self).__init__(*args, **kwargs)
 
