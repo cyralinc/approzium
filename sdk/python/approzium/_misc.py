fa0efee9f56a21cf8052a0dc5e98ac5c405c3213
Dio Gado <ahmedehabg@gmail.com>
2020-07-15 13:37:53 -0400

    Add Support for PyMySQL (#148)
    * Fix disable_tls=True
    
    * Fix Psycopg2 example
    
    * refactor
    
    * add pymysql support
    
    * Update poetry files
    
    * Add PyMySql example
    
    * Automatic lint
    
    Signed-off-by: GitHub Actions Bot <actions@github.com>
    
    * Add check
    
    * rename example
    
    * Add pymysql test
    
    * Add docstring and using default authclient
    
    * Automatic lint
    
    Signed-off-by: GitHub Actions Bot <actions@github.com>
    
    * add pymysql to docs site
    
    * Update compatibility pages
    
    * Fix typo sdk/python/approzium/pymysql/_connect.py
    
    Co-authored-by: Becca Petrin <beccapetrin@gmail.com>
    
    Co-authored-by: UpGado <UpGado@users.noreply.github.com>
    Co-authored-by: Becca Petrin <beccapetrin@gmail.com>

--- sdk/python/approzium/_misc.py
+++ sdk/python/approzium/_misc.py
@@ -1,5 +1,6 @@
 import logging
 import struct
+from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
 
@@ -7,3 +8,13 @@ logger = logging.getLogger(__name__)
 def read_int32_from_bytes(bytes, index):
     num = struct.unpack("!i", bytes[index : index + 4])[0]
     return num
+
+
+@contextmanager
+def patch(module, attribute_name, new_value):
+    original_value = getattr(module, attribute_name)
+    setattr(module, attribute_name, new_value)
+    try:
+        yield
+    finally:
+        setattr(module, attribute_name, original_value)
--- sdk/python/approzium/_mysql/__init__.py
+++ sdk/python/approzium/_mysql/__init__.py
@@ -3,12 +3,15 @@ import struct
 MYSQLNativePassword = "mysql_native_password"
 
 
-def get_auth_resp(
-    authenticator, dbhost, dbport, dbuser, auth_plugin, auth_data, is_secure_connection
-):
+def get_auth_resp(authenticator, dbhost, dbport, dbuser, auth_plugin, auth_data):
     plugin_auth_response = authenticator._get_mysql_hash(
         dbhost, dbport, dbuser, auth_plugin, auth_data
     )
+    return plugin_auth_response
+
+
+def get_auth_resp_msg(is_secure_connection, *args, **kwargs):
+    plugin_auth_response = get_auth_resp(*args, **kwargs)
     if is_secure_connection:
         resplen = len(plugin_auth_response)
         auth_response = struct.pack("<B", resplen) + plugin_auth_response
