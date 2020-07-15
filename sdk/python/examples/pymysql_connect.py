471a546201d5cdd66c054561156ebcb0c12f37f8
Ahmed Gado <ahmedehabg@gmail.com>
2020-07-15 00:32:03 -0400

    Add PyMySql example

---
+++ sdk/python/examples/pymysql_connect.py
@@ -0,0 +1,15 @@
+from approzium import AuthClient
+from approzium.pymysql import connect
+
+auth = AuthClient(
+    "authenticator:6001",
+    # This is insecure, see https://approzium.org/configuration for proper use.
+    disable_tls=True
+)
+conn = connect(host='dbmysqlsha1', user='bob', db='db',
+               authenticator=auth)
+with conn.cursor() as cursor:
+    cursor.execute('SELECT 1')
+    result = cursor.fetchone()
+    print(result)
+conn.close()
