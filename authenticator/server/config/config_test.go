f494fc75528f2e466834db066174cba08f418e60
Dio Gado <ahmedehabg@gmail.com>
2020-07-23 04:04:09 -0400

    Update config_test.go

--- authenticator/server/config/config_test.go
+++ authenticator/server/config/config_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestParseConfig(t *testing.T) {
 	os.Unsetenv("APPROZIUM_HOST")
-	os.Unsetenv("APPROZIUM_HTTP_PORT")
+	os.Unsetenv("APPROZIUM_PORT")
 	os.Unsetenv("APPROZIUM_LOG_LEVEL")
 	os.Setenv("APPROZIUM_DISABLE_TLS", "true")
 	config, err := Parse()
@@ -17,15 +17,15 @@ func TestParseConfig(t *testing.T) {
 	if config.Host != "127.0.0.1" {
 		t.Fatalf("expected %s, received %s", "127.0.0.1", config.Host)
 	}
-	if config.HTTPPort != 6000 {
-		t.Fatalf("expected %d, received %d", 6000, config.HTTPPort)
+	if config.Port != 6000 {
+		t.Fatalf("expected %d, received %d", 6000, config.Port)
 	}
 	if config.LogLevel != "info" {
 		t.Fatalf("expected %s, received %s", "info", config.LogLevel)
 	}
 
 	os.Setenv("APPROZIUM_HOST", "0.0.0.0")
-	os.Setenv("APPROZIUM_HTTP_PORT", "6001")
+	os.Setenv("APPROZIUM_PORT", "6001")
 	os.Setenv("APPROZIUM_LOG_LEVEL", "debug")
 	os.Setenv("APPROZIUM_DISABLE_TLS", "true")
 	config, err = Parse()
@@ -35,8 +35,8 @@ func TestParseConfig(t *testing.T) {
 	if config.Host != "0.0.0.0" {
 		t.Fatalf("expected %s, received %s", "0.0.0.0", config.Host)
 	}
-	if config.HTTPPort != 6001 {
-		t.Fatalf("expected %d, received %d", 6001, config.HTTPPort)
+	if config.Port != 6001 {
+		t.Fatalf("expected %d, received %d", 6001, config.Port)
 	}
 	if config.LogLevel != "debug" {
 		t.Fatalf("expected %s, received %s", "debug", config.LogLevel)
