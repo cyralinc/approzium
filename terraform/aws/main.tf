0dbd915bd0405bcab8c0ebc8edd216df5163041e
Ahmed Gado <ahmedehabg@gmail.com>
2020-07-24 15:59:45 -0400

    Remove SSH and add GRPC

--- terraform/aws/main.tf
+++ terraform/aws/main.tf
@@ -24,19 +24,10 @@ resource "aws_instance" "approzium" {
 }
 
 
-// Security group for Approzium allows SSH, HTTP, and GRPC access
+// Security group for Approzium allows HTTP, and GRPC access
 resource "aws_security_group" "approzium" {
     name = "approzium"
     description = "Approzium servers"
-}
-
-resource "aws_security_group_rule" "approzium-ssh" {
-    security_group_id = aws_security_group.approzium.id
-    type = "ingress"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group_rule" "approzium-http-api" {
@@ -44,6 +35,15 @@ resource "aws_security_group_rule" "approzium-http-api" {
     type = "ingress"
     from_port = 6000
     to_port = 6000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group_rule" "approzium-grpc" {
+    security_group_id = aws_security_group.approzium.id
+    type = "ingress"
+    from_port = 6001
+    to_port = 6001
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
 }
