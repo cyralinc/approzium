412195485ee5f91833614ec5b7714a99f47635fd
Becca Petrin <beccapetrin@gmail.com>
2020-08-06 10:19:31 -0700

    Refactor config parsing (#180)
    * Refactor config parsing
    
    * Handle version flag
    
    * Make flags take precedence over env
    
    * Correct yaml sample and parsing
    
    * Update test env vars
    
    * Finish updating AWS_REGION
    
    * Support env vars without APPROZIUM_ prefix

--- authenticator/server/config/config.go
+++ authenticator/server/config/config.go
@@ -2,11 +2,112 @@ package config
 
 import (
 	"errors"
+	"flag"
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
-
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
+	"reflect"
+	"strconv"
+	"strings"
 )
+
+const envVarPrefix = "APPROZIUM_"
+
+type section string
+
+const (
+	// For fields that shouldn't be included in the file conf
+	sectionExclude section = "exclude"
+
+	// For fields configuring how Approzium listens.
+	sectionListener = "listener"
+
+	// For fields configuring logging.
+	sectionLogging = "logging"
+
+	// For fields configuring TLS.
+	sectionTLS = "tls"
+
+	// For fields configuring a secrets manager.
+	sectionSecretsMgr = "secrets_manager"
+)
+
+var (
+	flagConf = &Config{}
+
+	// Add new fields to the config by simply adding them to the fieldRegistry. This automatically adds support
+	// for them in env vars, config files, command-line flags, and defaults.
+	fieldRegistry = map[section][]field{
+		sectionListener: {
+			{name: "host", defaultVal: "127.0.0.1", flagConfField: &flagConf.Host, goFieldName: "Host", prependEnvVar: true},
+			{name: "http port", defaultVal: 6000, flagConfField: &flagConf.HTTPPort, goFieldName: "HTTPPort", prependEnvVar: true},
+			{name: "grpc port", defaultVal: 6001, flagConfField: &flagConf.GRPCPort, goFieldName: "GRPCPort", prependEnvVar: true},
+		},
+		sectionLogging: {
+			{name: "log level", defaultVal: "info", flagConfField: &flagConf.LogLevel, goFieldName: "LogLevel", prependEnvVar: true},
+			{name: "log format", defaultVal: "text", flagConfField: &flagConf.LogFormat, goFieldName: "LogFormat", prependEnvVar: true},
+			{name: "log raw", defaultVal: false, flagConfField: &flagConf.LogRaw, goFieldName: "LogRaw", prependEnvVar: true},
+		},
+		sectionTLS: {
+			{name: "disable tls", defaultVal: false, flagConfField: &flagConf.DisableTLS, goFieldName: "DisableTLS", prependEnvVar: true},
+			{name: "tls cert path", defaultVal: "", flagConfField: &flagConf.PathToTLSCert, goFieldName: "PathToTLSCert", prependEnvVar: true},
+			{name: "tls key path", defaultVal: "", flagConfField: &flagConf.PathToTLSKey, goFieldName: "PathToTLSKey", prependEnvVar: true},
+		},
+		sectionSecretsMgr: {
+			{name: "secrets manager", defaultVal: "", flagConfField: &flagConf.SecretsManager, goFieldName: "SecretsManager", prependEnvVar: true},
+			{name: "vault token", defaultVal: "", flagConfField: &flagConf.VaultToken, goFieldName: "VaultToken", prependEnvVar: false},
+			{name: "vault token path", defaultVal: "", flagConfField: &flagConf.VaultTokenPath, goFieldName: "VaultTokenPath", prependEnvVar: true},
+			{name: "vault addr", defaultVal: "", flagConfField: &flagConf.VaultAddr, goFieldName: "VaultAddr", prependEnvVar: false},
+			{name: "aws region", defaultVal: "", flagConfField: &flagConf.AwsRegion, goFieldName: "AwsRegion", prependEnvVar: false},
+		},
+		sectionExclude: {
+			{name: "config", defaultVal: "", flagConfField: &flagConf.ConfigFilePath, goFieldName: "ConfigFilePath", prependEnvVar: true},
+			{name: "dev", defaultVal: false, flagConfField: &flagConf.DevMode, goFieldName: "DevMode", prependEnvVar: true},
+			{name: "version", defaultVal: false, flagConfField: &flagConf.Version, goFieldName: "Version", prependEnvVar: true},
+		},
+	}
+
+	allRegisteredFields = func() []field {
+		var allFields []field
+		for _, sectionFields := range fieldRegistry {
+			for _, registeredField := range sectionFields {
+				allFields = append(allFields, registeredField)
+			}
+		}
+		return allFields
+	}()
+)
+
+type field struct {
+	// A user-friendly name for the field. Multi-word parameters should be separated by spaces: "fizz buzz".
+	name string
+
+	// The default value for the field.
+	defaultVal interface{}
+
+	// The field on the flag conf to populate with this value.
+	flagConfField interface{}
+
+	// The name of the Go field this should be bound with.
+	goFieldName string
+
+	// Whether to prepend this option's env var with APPROZIUM_ when looking for it.
+	prependEnvVar bool
+}
+
+func init() {
+	for _, registeredField := range allRegisteredFields {
+		switch fieldOnConf := registeredField.flagConfField.(type) {
+		case *bool:
+			flag.BoolVar(fieldOnConf, flagName(registeredField.name), registeredField.defaultVal.(bool), "")
+		case *int:
+			flag.IntVar(fieldOnConf, flagName(registeredField.name), registeredField.defaultVal.(int), "")
+		case *string:
+			flag.StringVar(fieldOnConf, flagName(registeredField.name), registeredField.defaultVal.(string), "")
+		}
+	}
+}
 
 // Config is an object for storing configuration variables set through
 // Approzium's environment.
@@ -27,79 +128,22 @@ type Config struct {
 	LogRaw    bool
 
 	SecretsManager string
-	ConfigFilePath string
 
-	// Special flags
-	devMode bool
-	Version bool
-
-	// Vault specific flags
+	// Vault related
 	VaultToken     string
 	VaultTokenPath string
 	VaultAddr      string
 
-	// AWS Secrets Manager specific flags
+	// AWS secrets manager related
 	AwsRegion string
+
+	// Special flags
+	ConfigFilePath string
+	DevMode        bool
+	Version        bool
 }
 
-// Parse returns the parsed config. A pointer is not returned
-// because after first parse, the config is immutable.
-func Parse() (Config, error) {
-	config, err := parse()
-	if err != nil {
-		return Config{}, err
-	}
-	if err := verify(config); err != nil {
-		return Config{}, err
-	}
-	if config.devMode {
-		// dev mode uses the file back-end
-		if err := os.Unsetenv("VAULT_ADDR"); err != nil {
-			return Config{}, err
-		}
-		config = Config{
-			Host:       "127.0.0.1",
-			HTTPPort:   6000,
-			GRPCPort:   6001,
-			DisableTLS: true,
-			LogLevel:   "debug",
-			LogFormat:  "text",
-			LogRaw:     false,
-		}
-	}
-	return config, nil
-}
-
-func parse() (Config, error) {
-	var config Config
-	setConfigDefaults()
-	if err := setConfigFlags(); err != nil {
-		return Config{}, err
-	}
-	if err := setConfigEnvVars(); err != nil {
-		return Config{}, err
-	}
-	if err := viper.Unmarshal(&config); err != nil {
-		return Config{}, err
-	}
-	if config.ConfigFilePath == "" {
-		return config, nil
-	}
-	viper.SetConfigName("approzium.config")
-	viper.SetConfigType("yaml")
-	viper.AddConfigPath(config.ConfigFilePath)
-
-	// Find and read the config file
-	if err := viper.ReadInConfig(); err != nil {
-		return Config{}, err
-	}
-	if err := viper.Unmarshal(&config); err != nil {
-		return Config{}, err
-	}
-	return config, nil
-}
-
-func verify(config Config) error {
+func verify(config *Config) error {
 	if !config.DisableTLS {
 		if config.PathToTLSCert == "" {
 			return errors.New("tls is enabled but no tls cert has been provided")
@@ -111,110 +155,203 @@ func verify(config Config) error {
 	return nil
 }
 
-func setConfigEnvVars() error {
-	viper.SetEnvPrefix("approzium")
-	if err := viper.BindEnv("Host", "APPROZIUM_HOST"); err != nil {
-		return err
+// Parse returns the parsed config. A pointer is not returned
+// because after first parse, the config is immutable.
+func Parse() (Config, error) {
+	config, err := parseWithPrecedence()
+	if err != nil {
+		return Config{}, err
 	}
-	if err := viper.BindEnv("HTTPPort", "APPROZIUM_HTTP_PORT"); err != nil {
-		return err
+	if config.Version {
+		// Nothing further to do here.
+		return *config, nil
 	}
-	if err := viper.BindEnv("GRPCPort", "APPROZIUM_GRPC_PORT"); err != nil {
+	if config.DevMode {
+		config, err = devModeConfig()
+		if err != nil {
+			return Config{}, err
+		}
+	}
+	if err := verify(config); err != nil {
+		return Config{}, err
+	}
+	// We return a non-pointer to signal that it will never change.
+	return *config, nil
+}
+
+func parseWithPrecedence() (*Config, error) {
+	flag.Parse()
+
+	// Our order of precedence is:
+	// 1. Command-line flag
+	// 2. Env var
+	// 3. Config file
+	// 4. Default
+	conf, err := defaultConfig()
+	if err != nil {
+		return nil, err
+	}
+	if flagConf.ConfigFilePath != "" {
+		if err := overrideWithFileConf(conf); err != nil {
+			return nil, err
+		}
+	}
+	if err := overrideWithEnvConf(conf); err != nil {
+		return nil, err
+	}
+	if err := overrideWithFlagConf(conf); err != nil {
+		return nil, err
+	}
+	return conf, nil
+}
+
+func defaultConfig() (*Config, error) {
+	conf := &Config{}
+	writableConfFields := reflect.ValueOf(conf).Elem()
+	for _, registeredField := range allRegisteredFields {
+		strDefault := fmt.Sprintf("%v", registeredField.defaultVal)
+		if err := setField(writableConfFields, registeredField, strDefault); err != nil {
+			return nil, err
+		}
+	}
+	return conf, nil
+}
+
+func overrideWithFileConf(conf *Config) error {
+	writableConfFields := reflect.ValueOf(conf).Elem()
+
+	body, err := ioutil.ReadFile(flagConf.ConfigFilePath)
+	if err != nil {
 		return err
 	}
 
-	if err := viper.BindEnv("DisableTLS", "APPROZIUM_DISABLE_TLS"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("PathToTLSCert", "APPROZIUM_PATH_TO_TLS_CERT"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("PathToTLSKey", "APPROZIUM_PATH_TO_TLS_KEY"); err != nil {
+	bodyMap := make(map[section]map[string]interface{})
+	if err := yaml.Unmarshal(body, &bodyMap); err != nil {
 		return err
 	}
 
-	if err := viper.BindEnv("LogLevel", "APPROZIUM_LOG_LEVEL"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("LogFormat", "APPROZIUM_LOG_FORMAT"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("LogRaw", "APPROZIUM_LOG_RAW"); err != nil {
-		return err
-	}
-	if err := viper.BindEnv("SecretsManager", "APPROZIUM_SECRETS_MANAGER"); err != nil {
-		return err
-	}
+	for section, registeredFields := range fieldRegistry {
+		if section == sectionExclude {
+			continue
+		}
+		sectionFields, exists := bodyMap[section]
+		if !exists {
+			continue
+		}
 
-	if err := viper.BindEnv("ConfigFilePath", "APPROZIUM_CONFIG_FILE_PATH"); err != nil {
-		return err
-	}
-
-	if err := viper.BindEnv("VaultTokenPath", "APPROZIUM_VAULT_TOKEN_PATH"); err != nil {
-		return err
-	}
-
-	if err := viper.BindEnv("VaultToken", "VAULT_TOKEN"); err != nil {
-		return err
-	}
-
-	if err := viper.BindEnv("VaultAddr", "VAULT_ADDR"); err != nil {
-		return err
-	}
-
-	if err := viper.BindEnv("AwsRegion", "AWS_REGION"); err != nil {
-		return err
+		for _, registeredField := range registeredFields {
+			fieldIfc, exists := sectionFields[fileName(registeredField.name)]
+			if !exists {
+				continue
+			}
+			strVal := fmt.Sprintf("%v", fieldIfc)
+			if err := setField(writableConfFields, registeredField, strVal); err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
 
-func setConfigDefaults() {
-	viper.SetDefault("Host", "127.0.0.1")
-	viper.SetDefault("HTTPPort", "6000")
-	viper.SetDefault("GRPCPort", "6001")
-
-	viper.SetDefault("DisableTLS", false)
-
-	viper.SetDefault("LogLevel", "info")
-	viper.SetDefault("LogFormat", "text")
-	viper.SetDefault("LogRaw", false)
-	viper.SetDefault("SecretsManager", "")
+// overrideWithFlagConf returns any command-line flags that were set in,
+// or default values.
+func overrideWithFlagConf(conf *Config) error {
+	writableConfFields := reflect.ValueOf(conf).Elem()
+	for _, registeredField := range allRegisteredFields {
+		f, found := getFlag(flagName(registeredField.name))
+		if !found {
+			continue
+		}
+		if err := setField(writableConfFields, registeredField, f.Value.String()); err != nil {
+			return err
+		}
+	}
+	return nil
 }
 
-func setConfigFlags() error {
-	// avoid redefining flags because it leads to panic
-	if pflag.Lookup("host") == nil {
-		pflag.String("host", "", "")
-		pflag.String("httpport", "", "")
-		pflag.String("grpcport", "", "")
+func overrideWithEnvConf(conf *Config) error {
+	writableConfFields := reflect.ValueOf(conf).Elem()
+	for _, registeredField := range allRegisteredFields {
+		fieldValue := os.Getenv(envVarName(registeredField))
+		if fieldValue == "" {
+			continue
+		}
+		if err := setField(writableConfFields, registeredField, fieldValue); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
-		pflag.Bool("disabletls", false, "")
-		pflag.String("tlscertpath", "", "")
-		pflag.String("tlskeypath", "", "")
+func devModeConfig() (*Config, error) {
+	// dev mode uses the local file back-end
+	if err := os.Unsetenv("VAULT_ADDR"); err != nil {
+		return &Config{}, err
+	}
+	return &Config{
+		Host:       "127.0.0.1",
+		HTTPPort:   6000,
+		GRPCPort:   6001,
+		DisableTLS: true,
+		LogLevel:   "debug",
+		LogFormat:  "text",
+		LogRaw:     false,
+		DevMode:    true,
+	}, nil
+}
 
-		pflag.String("loglevel", "", "")
-		pflag.String("logformat", "", "")
-		pflag.Bool("lograw", false, "")
+func envVarName(f field) string {
+	envVar := strings.ToUpper(strings.ReplaceAll(f.name, " ", "_"))
+	if !f.prependEnvVar {
+		return envVar
+	}
+	return envVarPrefix + envVar
+}
 
-		pflag.String("secretsmanager", "", "")
-		pflag.String("config", "", "")
+func fileName(name string) string {
+	return strings.ReplaceAll(name, " ", "_")
+}
 
-		pflag.Bool("dev", false, "whether to run the authenticator in dev mode")
-		pflag.Bool("version", false, "output the current authenticator version")
+func flagName(name string) string {
+	return strings.ReplaceAll(name, " ", "-")
+}
 
-		pflag.String("vaulttokenpath", "", "")
-		pflag.String("vaulttoken", "", "")
-		pflag.String("vaultaddr", "", "")
+func getFlag(name string) (*flag.Flag, bool) {
+	toReturn := &flag.Flag{}
+	found := false
+	flag.Visit(func(f *flag.Flag) {
+		if f.Name == name {
+			toReturn = f
+			found = true
+		}
+	})
+	return toReturn, found
+}
 
-		pflag.String("awsregion", "", "")
+func setField(elem reflect.Value, registeredField field, value string) error {
+	field := elem.FieldByName(registeredField.goFieldName)
+
+	if reflect.DeepEqual(field, reflect.Value{}) {
+		return fmt.Errorf("unable to locate %s", registeredField.goFieldName)
 	}
 
-	pflag.Parse()
-	if err := viper.BindPFlags(pflag.CommandLine); err != nil {
-		return err
-	}
-	if err := viper.BindPFlag("configfilepath", pflag.Lookup("config")); err != nil {
-		return err
+	switch registeredField.flagConfField.(type) {
+	case *bool:
+		b, err := strconv.ParseBool(value)
+		if err != nil {
+			return err
+		}
+		field.SetBool(b)
+	case *int:
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			return err
+		}
+		field.SetInt(int64(i))
+	case *string:
+		field.SetString(value)
+	default:
+		return fmt.Errorf("unrecognized field type: %T", registeredField.flagConfField)
 	}
 	return nil
 }
