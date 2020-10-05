45d0dfae37e7741ba379f79082acc602b88df9db
Lorena Leão <42307217+lorenaleao@users.noreply.github.com>
2020-10-05 14:19:50 -0300

    Make the health check endpoint return more information (#204)
    * Make the health check endpoint return more info.
    
    Make it so the health check endpoint returns a body like
    {"server_time_utc": "2020-09-25T16:08:11Z", "version": "0.9.1"}.
    
    * Fix TestHealthChecker function

--- authenticator/server/api/health.go
+++ authenticator/server/api/health.go
@@ -2,6 +2,9 @@ package api
 
 import (
 	"net/http"
+	"time"
+
+	"encoding/json"
 
 	"github.com/cyralinc/approzium/authenticator/server/config"
 	log "github.com/sirupsen/logrus"
@@ -19,10 +22,27 @@ type healthChecker struct {
 	config config.Config
 }
 
+const currentVersion = "0.2.0"
+
+type healthResponse struct {
+	ServerTime string `json:"server_time_utc"`
+	Version    string `json:"version"`
+}
+
 // For most things that use a health check to determine if a service is up,
 // like for AWS and Kubernetes for instance, a service is considered unhealthy
 // if it returns anything other than a 200.
 func (h *healthChecker) ServeHTTP(wr http.ResponseWriter, _ *http.Request) {
 	h.logger.Debug("server is healthy")
-	wr.WriteHeader(200)
+
+	wr.WriteHeader(http.StatusOK)
+	wr.Header().Set("Content-Type", "application/json")
+	response := healthResponse{
+		ServerTime: time.Now().UTC().Format(time.RFC3339),
+		Version:    currentVersion,
+	}
+
+	h.logger.Debugf("healthResponse: %+v", response)
+
+	json.NewEncoder(wr).Encode(response)
 }
